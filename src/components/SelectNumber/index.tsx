be9d801c51017212f1e022f3986bad88095de65c
haisamarie <marie.199204@gmail.com>
2025-06-20 01:02:58 +0900

    SelectNumberコンポーネントにカウンターの上限設定機能を追加

--- src/components/SelectNumber/index.tsx
+++ src/components/SelectNumber/index.tsx
@@ -1,10 +1,19 @@
 import styles from "./index.module.css";
+import { useState, type ChangeEvent } from "react";
 export const SelectNumber = () => {
+  const [upperLimit, setUpperLimit] = useState<number>(0);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    setUpperLimit(value);
+  };
+
   return (
     <div className={styles["wrap"]}>
+      {upperLimit}
       <p className={styles["text"]}>カウンターの上限設定</p>
       <input
-        onChange={() => {}}
+        onChange={handleChange}
         className={styles["input"]}
         type="number"
         min={0}
