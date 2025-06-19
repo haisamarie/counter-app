f54de43e5b53eb546aa1025f491a3325548b5a32
haisamarie <marie.199204@gmail.com>
2025-06-19 20:52:23 +0900

    Appコンポーネントを新規作成し、スタイルをCSSモジュールに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import "./App.css";
-
+import styles from "./App.module.css";
 function App() {
   return (
     <>
-      <div>aaa</div>
+      <div>
+        <h2>カウンター</h2>
+        <p>
+          カウンターの値: <span>0</span>
+        </p>
+        <div>
+          <button>増やす</button>
+          <button>減らす</button>
+          <button>リセット</button>
+        </div>
+      </div>
     </>
   );
 }
