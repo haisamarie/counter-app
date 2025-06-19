b3c41f3bf02af4f079fc17646732417bca6661ad
haisamarie <marie.199204@gmail.com>
2025-06-19 23:59:24 +0900

    onClickCountDown関数のsetCount呼び出しをコールバック関数に統一

--- src/hooks/useCount.ts
+++ src/hooks/useCount.ts
@@ -7,7 +7,6 @@ export const useCount = () => {
     setCount((prev) => prev + 1);
   };
   const onClickCountDown = () => {
-    setCount(count - 1);
     setCount((prev) => prev - 1);
   };
   const onClickClear = () => {
